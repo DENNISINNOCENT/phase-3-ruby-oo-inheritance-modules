e8267f0e56278c11ee10bb1d1f3f25319dc38390
DENNISINNOCENT <mutindahdennis@gmail.com>
2022-09-02 01:30:01 +0300

    commit tree 9daa11593a46efe72b88f13194c285c39f66a977

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,16 +1,16 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
 require_relative './fancy_dance'
-class Dancer
-  include Dance
-   extend MetaDancing
+# class Dancer
+#   include Dance
+#    extend MetaDancing
 
-  attr_accessor :name
+#   attr_accessor :name
 
-  def initialize(name)
-    @name = name
-  end
-end
+#   def initialize(name)
+#     @name = name
+#   end
+# end
 class Dancer
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
